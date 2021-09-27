53faed37fa14c6b57104932d6005e258e3a595eb
eredotpkfr <erdoganyoksul3@gmail.com>
2021-09-27 15:09:26 +0300

    fix: improved code readability, basically changed parameter names

--- hashes.go
+++ hashes.go
@@ -3,26 +3,26 @@ package golidators
 import "github.com/eredotpkfr/golidators/internal/regexes"
 
 // Md5 function for validating MD5
-func Md5(hash string) bool {
-	return regexes.Md5Regex.MatchString(hash)
+func Md5(md5 string) bool {
+	return regexes.Md5Regex.MatchString(md5)
 }
 
 // Sha1 function for validating SHA1
-func Sha1(hash string) bool {
-	return regexes.Sha1Regex.MatchString(hash)
+func Sha1(sha1 string) bool {
+	return regexes.Sha1Regex.MatchString(sha1)
 }
 
 // Sha224 function for validating SHA224
-func Sha224(hash string) bool {
-	return regexes.Sha224Regex.MatchString(hash)
+func Sha224(sha224 string) bool {
+	return regexes.Sha224Regex.MatchString(sha224)
 }
 
 // Sha256 function for validating SHA256
-func Sha256(hash string) bool {
-	return regexes.Sha256Regex.MatchString(hash)
+func Sha256(sha256 string) bool {
+	return regexes.Sha256Regex.MatchString(sha256)
 }
 
 // Sha512 function for validating SHA512
-func Sha512(hash string) bool {
-	return regexes.Sha512Regex.MatchString(hash)
+func Sha512(sha512 string) bool {
+	return regexes.Sha512Regex.MatchString(sha512)
 }
